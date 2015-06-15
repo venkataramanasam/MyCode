e6d0e5562602e8fa006fb0b816d95227abb0a1ae
venkataramanasam <venkataramanasam@users.noreply.github.com>
2015-06-16 02:12:20 +0530

    Update Raspberry.java

--- Raspberry.java
+++ Raspberry.java
@@ -3,11 +3,11 @@
  * and open the template in the editor.
  */
 
-
 /**
  *
  * @author sam
  */
+
 import java.util.*;
 import javax.mail.*;
 import javax.mail.Flags.Flag;
@@ -21,9 +21,14 @@ import com.pi4j.io.gpio.RaspiPin;
 
 public class Raspberry {
     public static void main(String[] args) {
-        Properties props = new Properties();
-        props.setProperty("mail.store.protocol", "imaps");
+
+        while(true)
+        {
+
+        System.out.println("In a loop");
         try {
+            Properties props = new Properties();
+            props.setProperty("mail.store.protocol", "imaps");
             Session session = Session.getInstance(props, null);
             Store store = session.getStore();
             store.connect("imap.gmail.com", "raspberrypivenkatsam@gmail.com", "pipassword");
@@ -31,19 +36,18 @@ public class Raspberry {
             inbox.open(Folder.READ_WRITE);
             Message[] msg = inbox.search(new FlagTerm(new Flags(Flag.SEEN), false));
 
-     
+
       for (int i = 0, n = msg.length; i < n; i++) {
          Message message = msg[i];
-         
-         if(message.getFrom()[0].toString().contains("samvenkatram"))
+if(message.getFrom()[0].toString().contains("samvenkatram"))
          {
              System.out.println("<--Pi4J--> GPIO Control Example ... started.");
-        
+
         // create gpio controller
-        final GpioController gpio = GpioFactory.getInstance();
-        
+         GpioController gpio = GpioFactory.getInstance();
+
         // provision gpio pin #01 as an output pin and turn on
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+         GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
 
         // set shutdown state for this pin
         pin.setShutdownOptions(true, PinState.LOW);
@@ -51,7 +55,7 @@ public class Raspberry {
         System.out.println("--> GPIO state should be: ON");
 
         Thread.sleep(5000);
-        
+
         // turn off gpio pin #01
         pin.low();
         System.out.println("--> GPIO state should be: OFF");
@@ -67,24 +71,27 @@ public class Raspberry {
         // toggle the current state of gpio pin #01  (should turn off)
         pin.toggle();
         System.out.println("--> GPIO state should be: OFF");
-        
+
         Thread.sleep(5000);
 
         // turn on gpio pin #01 for 1 second and then off
         System.out.println("--> GPIO state should be: ON for only 1 second");
         pin.pulse(1000, true); // set second argument to 'true' use a blocking call
-        
+
         // stop all GPIO activity/threads by shutting down the GPIO controller
-        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
+// (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
         gpio.shutdown();
+        gpio.unprovisionPin(pin);
         message.setFlag(Flags.Flag.SEEN, true);
          }
-             
-         
-         
 
-      }        } catch (Exception mex) {
+         }
+
+      Thread.sleep(20000);
+        } catch (Exception mex) {
             mex.printStackTrace();
+        }
+
         }
     }
 }
